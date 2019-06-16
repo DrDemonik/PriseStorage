e70238e54f0b5af3c3992e9063f779853f80b462
Некряч Дмитрий <d.nekryach@forte.su>
2019-06-16 17:20:45 +0300

    Расчет стоимости хранения грузов

--- PriceStorage/Form1.Designer.cs
+++ PriceStorage/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Exsel file (*.xlsx)|*.*";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 94);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 10, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(218, 94);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.Value = new System.DateTime(2017, 10, 1, 0, 0, 0, 0);
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(923, 363);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Загрузить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Расчитайть данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Дата начала периода хранения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Дата окончания периода хранения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(947, 495);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PriceStorage/Form1.cs
+++ PriceStorage/Form1.cs
@@ -7,14 +7,167 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
 
 namespace PriceStorage
 {
     public partial class Form1 : Form
     {
+        System.Data.DataTable tableGryz;
+        System.Data.DataTable tableTarif;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    tableGryz = ConvertExcelToDataTable(openFileDialog1.FileName, "Груз");
+                    tableTarif = ConvertExcelToDataTable(openFileDialog1.FileName, "Тариф");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                
+                if (tableGryz != null && tableTarif != null)
+                {
+                    var dataTableOut = tableGryz.Clone();
+                    dataTableOut.Columns.Add("Начало периода", typeof(System.DateTime));
+                    dataTableOut.Columns.Add("Окончание периода", typeof(System.DateTime));
+                    dataTableOut.Columns.Add("Кол-во дней хранения", typeof(System.Int32));
+                    dataTableOut.Columns.Add("Ставка", typeof(System.Int32));
+                    dataTableOut.Columns.Add("Сумма", typeof(System.Double));
+
+                    foreach (var drGryz in tableGryz.Select())
+                    {
+                        var startDate = dateTimePicker1.Value;
+                        var endDate = dateTimePicker2.Value;
+                        endDate = new DateTime(endDate.Year, endDate.Month, endDate.Day, 23, 59, 59);
+
+                        var minDate = drGryz[1] != System.DBNull.Value ? Convert.ToDateTime(drGryz[1]) : new DateTime(1,12,1);
+                        if (startDate < minDate)
+                        {
+                            startDate = minDate;
+                        }
+
+                        var maxDate = drGryz[2] != System.DBNull.Value ? Convert.ToDateTime(drGryz[2]) : new DateTime(9999, 12, 1);
+                        if (endDate > maxDate)
+                        {
+                            endDate = maxDate;
+                        }
+                        if (endDate < startDate)
+                        {
+                            endDate = startDate;
+                        }
+
+                        var deltatime = endDate - startDate;
+                        var countDay = Math.Floor(deltatime.TotalDays);
+                        double sum=0;
+                        Int32 stavka = 0;
+                        foreach (var drTarif in tableTarif.Select())
+                        {
+                            var maxDay = drTarif[2] != System.DBNull.Value ? (double)drTarif[2] : countDay;
+                            if (countDay > (double)drTarif[1] && countDay >= maxDay)
+                            {
+                                var s = (maxDay - (double)drTarif[1]) * (double)drTarif[3];
+                                sum += s;
+                                stavka = Convert.ToInt32(drTarif[3]);
+                            }
+                            else if (countDay >= (double)drTarif[1] && countDay < maxDay)
+                            {
+                                var s = (countDay - ((double)drTarif[1]-1)) * (double)drTarif[3];
+                                sum += s;
+                                stavka = Convert.ToInt32(drTarif[3]);
+                            }
+                        }
+
+                        var desRow = dataTableOut.NewRow();
+                        desRow.ItemArray = drGryz.ItemArray.Clone() as object[];
+                        desRow["Начало периода"] = startDate;
+                        desRow["Окончание периода"] = endDate;
+                        desRow["Кол-во дней хранения"] = (Int32)countDay;
+                        desRow["Ставка"] = stavka;
+                        desRow["Сумма"] = sum;
+                        dataTableOut.Rows.Add(desRow);
+
+                    }
+                    dataGridView1.DataSource = dataTableOut;
+                }
+                else
+                {
+                    throw new Exception("Данные в таблицах отсутвуют. Загрузите данные из файла!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }   
+
+        public static System.Data.DataTable ConvertExcelToDataTable(string fileName, string mySheetName="")
+        {
+            System.Data.DataTable dtResult = null;
+            int totalSheet = 0;  
+            using (OleDbConnection objConn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=YES;IMEX=1;';"))
+            {
+                objConn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                OleDbDataAdapter oleda = new OleDbDataAdapter();
+                DataSet ds = new DataSet();
+                System.Data.DataTable dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = string.Empty;
+                if (dt != null)
+                {
+                    var tempDataTable = (from dataRow in dt.AsEnumerable()
+                                         where !dataRow["TABLE_NAME"].ToString().Contains("FilterDatabase")
+                                         && dataRow["TABLE_NAME"].ToString().Contains(mySheetName)
+                                         select dataRow)?.CopyToDataTable();
+                    dt = tempDataTable;
+                    totalSheet = dt.Rows.Count;
+                    sheetName = dt.Rows[0]["TABLE_NAME"].ToString();
+                }
+                cmd.Connection = objConn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+                oleda = new OleDbDataAdapter(cmd);
+                oleda.Fill(ds, "excelData");
+                dtResult = ds.Tables["excelData"];
+                objConn.Close();
+                return dtResult; 
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value > dateTimePicker2.Value)
+            {
+                dateTimePicker2.Value = dateTimePicker1.Value;
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            if (dateTimePicker2.Value < dateTimePicker1.Value)
+            {
+                dateTimePicker1.Value = dateTimePicker2.Value;
+            }
         }
     }
 }
